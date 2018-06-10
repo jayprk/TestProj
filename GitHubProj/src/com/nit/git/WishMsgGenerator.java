83741560bcc76be285761e18f20a9b080b85bc77
jay <jay@Jay>
2018-06-10 20:30:40 +0530

    jani first commit

--- GitHubProj/src/com/nit/git/WishMsgGenerator.java
+++ GitHubProj/src/com/nit/git/WishMsgGenerator.java
@@ -5,5 +5,7 @@ public class WishMsgGenerator {
 	public void wishMsg(String name){
 		System.out.println("Good Morning "+ name);
 	}
-
+	public int sum(int a,int b){
+		return a+b;
+	}
 }
--- GitHubProj/src/com/nit/test/GitTest.java
+++ GitHubProj/src/com/nit/test/GitTest.java
@@ -6,7 +6,9 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		WishMsgGenerator msg = new WishMsgGenerator();
-		msg.wishMsg("jay");
+		msg.wishMsg("jay");	
+		
+		msg.sum(10,20);
 	}
-
+	
 }
