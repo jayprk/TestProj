f0f9607897945c76acfdcb7cdac0495be804b57f
jay <jay@Jay>
2018-06-10 20:08:49 +0530

    initial commit

---
+++ GitHubProj/src/com/nit/git/WishMsgGenerator.java
@@ -0,0 +1,9 @@
+package com.nit.git;
+
+public class WishMsgGenerator {
+	
+	public void wishMsg(String name){
+		System.out.println("Good Morning "+ name);
+	}
+
+}
---
+++ GitHubProj/src/com/nit/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.nit.test;
+
+import com.nit.git.WishMsgGenerator;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		WishMsgGenerator msg = new WishMsgGenerator();
+		msg.wishMsg("jay");
+	}
+
+}
